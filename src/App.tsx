c4a5781b02f97812f1434df57a6ca929ad2db4a3
michelzaum <michel.oliveira999@hotmail.com>
2025-05-17 16:19:01 -0300

    feat: create AppBar component and hide the logout button if the user is not signed in

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
 
-import { LogOutIcon } from 'lucide-react';
 import { Router } from './Router';
-import { ThemeSwitcher } from './components/ThemeSwitcher';
-import { Button } from './components/ui/Button';
-import { Tooltip } from './components/ui/Tooltip';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { AuthProvider } from './contexts/AuthContext';
+import { AppBar } from './components/AppBar';
 
 export function App() {
   return (
     <AuthProvider>
       <ThemeProvider>
-        <div className="fixed right-4 top-4 flex items-center gap-4">
-          <ThemeSwitcher />
-
-          <Tooltip content="Sair">
-            <Button variant="secondary" size="icon" className="rounded-full">
-              <LogOutIcon className='w4 h-4' />
-            </Button>
-          </Tooltip>
-        </div>
+        <AppBar />
 
         <BrowserRouter>
           <Router />
