decbdbe305caa1acf4c4347e530cc48b56d1c924
michelzaum <michel.oliveira999@hotmail.com>
2025-05-17 22:09:14 -0300

    feat: use interceptor to send the access token in the requests

--- src/services/OrdersService.ts
+++ src/services/OrdersService.ts
@@ -1,16 +1,10 @@
 import { IOrder } from '@/entities/IOrder';
 import { httpClient } from './httpClient';
-import { storageKeys } from '@/config/storageKeys';
 
 export class OrdersService {
   static async getOrders() {
-    const accessToken = localStorage.getItem(storageKeys.accessToken);
+    const { data } = await httpClient.get<{ orders: IOrder[] }>('/orders');
 
-    const { data } = await httpClient.get<{ orders: IOrder[] }>('/orders', {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      }
-    });
     return data.orders;
   }
 }
